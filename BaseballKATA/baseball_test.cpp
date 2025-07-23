8716745cf6c78071d17ffc84694ddae862cc0546
sh1.lee <sh1.lee@samsung.com>
2025-07-23 13:11:38 +0900

    [feature] 27

--- BaseballKATA/baseball_test.cpp
+++ BaseballKATA/baseball_test.cpp
@@ -1,13 +1,7 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-    EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 1);
-}
-
-int main() {
-    ::testing::InitGoogleMock();
-    return RUN_ALL_TESTS();
+TEST(BaseballGame, ThrowExceptionWHenInputLengthIsUnmached) {
+    Baseball game;
+    EXPECT_THROW(game.guess(string("12")), length_error);
 }
