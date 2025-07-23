e45b2c2b3789ec4aaca356ad5384d3a000e4d546
sh1.lee <sh1.lee@samsung.com>
2025-07-23 13:53:03 +0900

    [feature] 49

--- BaseballKATA/baseball.cpp
+++ BaseballKATA/baseball.cpp
@@ -1,10 +1,23 @@
 #include <stdexcept>
 
 using namespace std;
+
+struct GuessResult {
+    bool solved;
+    int strikes;
+    int balls;
+};
 class Baseball {
 public:
-    void guess(const string& guessNumber) {
+public:
+
+    explicit Baseball(const string& question)
+        : question(question) {
+    }
+
+    GuessResult guess(const string& guessNumber) {
         assertIllegalArgument(guessNumber);
+        return { true, 3, 0 };
     }
 
     void assertIllegalArgument(const std::string& guessNumber)
@@ -27,4 +40,8 @@ public:
             || guessNumber[0] == guessNumber[2]
             || guessNumber[1] == guessNumber[2];
     }
+private:
+    string question;
+
+
 };
